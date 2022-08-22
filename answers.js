8d4503de80ec63c16c530bbd099eafd4a20edd5e
Jeonghyun Seo <jseo2022se@gmail.com>
2022-08-22 15:14:54 -0400

    Where is Waldo answered

--- answers.js
+++ answers.js
@@ -110,3 +110,14 @@
 
 // 12. no, as declaring const favMovies as an array means we can mutate it as an array, but cannot change favMovies to another type like String or Integer
 
+///////////////////////////////////////////////////////////////
+//// WHERE IS WALDO
+///////////////////////////////////////////////////////////////
+
+const whereIsWaldo = [["Timmy", "Frank"], "Eggbert", ["Lucinda", "Jacc", "Neff", "Snoop"], ["Petunia", ["Baked Goods","Waldo"]]];
+
+whereIsWaldo.splice(1,1);
+
+whereIsWaldo[1][2] = 'No One';
+
+console.log(whereIsWaldo[2][1][1]);
